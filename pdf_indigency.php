a78c1d1a4a3abedb99fb1f02f0ef458e48d65d37
Kurt013 <kurtvincentalmodovar65@gmail.com>
2025-01-25 09:07:03 +0800

    draft of all generated list

--- pdf_indigency.php
+++ pdf_indigency.php
@@ -3,9 +3,6 @@
 require './vendor/autoload.php';
 require './classes/main.class.php';
 
-
-
-
 if (isset($_POST['views_data'])) {
     $columns = json_decode($_POST['views_data'], true); 
 
@@ -21,31 +18,35 @@ if (isset($_POST['views_data'])) {
         // Add a header
         
         $pdf->SetFont('helvetica', 'B', 16);
-        $pdf->Cell(0, 15, 'Summarized Report', 0, 1, 'C');
+        $pdf->Cell(0, 15, 'Generated List', 0, 1, 'C');
         
 
         $pdf->Ln(5);
         $pdf->SetFont('helvetica', 'B', 12);
-        $pdf->Cell(35, 10, 'ID', 1, 0, 'C');
+        $pdf->Cell(35, 10, 'ID Indigency', 1, 0, 'C');
         $pdf->Cell(50, 10, 'First Name', 1, 0, 'C');
         $pdf->Cell(10, 10, 'Middle Name', 1, 0, 'C');
         $pdf->Cell(30, 10, 'Last Name', 1, 0, 'C');
         $pdf->Cell(20, 10, 'Suffix', 1, 0, 'C');
-        $pdf->Cell(40, 10, 'Business Name', 1, 0, 'C');
-        $pdf->Cell(40, 10, 'Industry', 1, 0, 'C');
-        $pdf->Cell(30, 10, 'AOE', 1, 1, 'C');
+        $pdf->Cell(20, 10, 'Nationality', 1, 0, 'C');
+        $pdf->Cell(20, 10, 'Age', 1, 0, 'C');
+        $pdf->Cell(30, 10, 'House No', 1, 0, 'C');
+        $pdf->Cell(40, 10, 'Street', 1, 0, 'C');
+        $pdf->Cell(40, 10, 'Purpose', 1, 0, 'C');
 
         // Add the rescert list
         $pdf->SetFont('helvetica', '', 12);
         foreach ($columns as $column) {
-            $pdf->Cell(35, 10, $column['id_bspermit'], 1, 0, 'L');
+            $pdf->Cell(35, 10, $column['id_indigency'], 1, 0, 'L');
             $pdf->Cell(50, 10, $column['fname'], 1, 0, 'L');
             $pdf->Cell(10, 10, $column['mi'], 1, 0, 'L');
             $pdf->Cell(30, 10, $column['lname'], 1, 0, 'L');
             $pdf->Cell(20, 10, $column['suffix'], 1, 0, 'L');
-            $pdf->Cell(40, 10, $column['bsname'], 1, 0, 'L');
-            $pdf->Cell(40, 10, $column['bsindustry'], 1, 0, 'L');
-            $pdf->Cell(30, 10, $column['aoe'] . ' sqm.', 1, 1, 'L');
+            $pdf->Cell(20, 10, $column['nationality'], 1, 0, 'L');
+            $pdf->Cell(20, 10, $column['age'], 1, 0, 'L');
+            $pdf->Cell(30, 10, $column['houseno'], 1, 0, 'L');
+            $pdf->Cell(40, 10, $column['street'], 1, 0, 'L');
+            $pdf->Cell(40, 10, $column['purpose'], 1, 0, 'L');
         }
 
         // Correct Output method to ensure proper file download
